b7039f07230625172e90b63c1f7ff3062975f7c8
Erika Weil <erikaweil@outlook.com>
2017-09-01 00:08:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  let current = document.getElementById('div#grand-node')
+  let current = document.getElementById('grand-node')
   let next = current.children[0];
   debugger;
   while (next) {
